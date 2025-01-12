8090e73f76a485cb612f822a4aeca872bf6aa62b
chandrahasa399 <147586327+chandrahasa399@users.noreply.github.com>
2025-01-12 08:22:54 +0000

    Submission for pe

---
+++ myresume/src/Resume.js
@@ -0,0 +1,48 @@
+
+import './Resume.css';
+function Resume() {
+  return (
+    <body>
+    <div id="header">
+        <h1>Chandrahasa Munagala</h1>
+        <p>Seattle,Washington,98118 | (253) 349-6520|
+            <br></br>
+        chandrahasa399@gmail.com
+        </p>
+        <h2>Education</h2>
+        <b>Master of Science in Computer Science</b>
+        <p>City University of Seattle,Seattle,Washington,98121</p>
+        <b>Bachelor of Technology in Computer Science </b>
+        <p>Sri Indu Institute of Engineering & Technology,Hyderabad|May 2020
+           <br></br>
+            GPA:7.86/10
+        </p>
+        <h2>Skills</h2>
+        <p>Programming Languages: C,Java,Python
+            <br></br>
+           Frameworks and libraries:React,Node.js
+           <br></br>
+           Database systems:MySQL
+           <br></br>
+           Operating systems:Windows,Linux
+        </p>
+        <h2>Work Experience</h2>
+        <b>Consultant</b>
+        <p>JKTech,Bengaluru|June 2021-June 2023 
+           <br></br>
+            Worked on project using front end,database technologies and resolved defects raised by testing team
+        </p>
+        <h2>Projects</h2>
+        <b> health Management System</b>
+        <br></br>
+        <p>which deals with health issues using Internet of Things</p>
+
+    </div>
+    </body>
+   
+  );
+  }
+  
+
+
+export default Resume;
